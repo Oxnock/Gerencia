83b13d28733cecebec862e8397c745f35d348123
Oxnock <jsezuniga72@gmail.com>
2019-03-06 17:41:13 -0600

    Mega Push
    Venid, Venid a por mi, pero con la cara destapada, venid a darte de hostias conmigo,
    HIJOS DE PUTA

---
+++ 01-login/Models/UserProfileModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleMvcApp.Models
+{
+    public class UserProfileModel
+    {
+        public string EmailAddress { get; set; }
+
+        public string Name { get; set; }
+
+        public string FirstLastName { get; set; }
+
+        public string SecondLastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string PersonID { get; set; }
+
+        public string IDType { get; set; }
+
+    }
+}
