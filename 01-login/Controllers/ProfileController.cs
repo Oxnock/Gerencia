a50f82988b35824bf3f0d8928c22a39e435c57ee
Oxnock <jsezuniga72@gmail.com>
2019-03-26 16:48:52 -0600

    Proyecto
    a

---
+++ 01-login/Controllers/ProfileController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleMvcApp.Data;
+
+namespace SampleMvcApp.Controllers
+{
+    public class ProfileController : Controller
+    {    
+
+        // GET: Profile
+        public ActionResult Index()
+        {
+            sql3283190Context context = HttpContext.RequestServices.GetService(typeof(sql3283190Context)) as sql3283190Context;
+
+            return View(context.ObtenerPersona(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value));
+
+            //return View();
+        }
+
+        // GET: Profile/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Profile/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profile/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult FORMINICIAL(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult FORMINICIAL(int id, IFormCollection collection)
+        {
+            try
+            {
+                
+
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Profile/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Profile/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
